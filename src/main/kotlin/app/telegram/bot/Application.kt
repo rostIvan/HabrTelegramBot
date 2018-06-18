f3d4963f2f36612d5fe7ed20dd8125533a9259a5
rost <bbwgd77@gmail.com>
2018-06-18 20:45:37 +0300

    Initial commit

---
+++ src/main/kotlin/app/telegram/bot/Application.kt
@@ -0,0 +1,10 @@
+package app.telegram.bot
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
---
+++ src/test/kotlin/app/telegram/bot/BotTest.kt
@@ -0,0 +1,21 @@
+package app.telegram.bot
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BotTest {
+    @Before
+    fun before() {  }
+
+    @Test
+    fun test() {  }
+
+    @After
+    fun after() {  }
+}
